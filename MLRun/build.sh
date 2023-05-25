a5894a46aabd81d5d1164d38ba81e89d46263c0d
Balde, Thierno Abdourahmane <Thierno-Abdourahmane.Balde@adesso-insurance-solutions.de>
2023-05-25 14:54:57 +0200

    new path

--- MLRun/build.sh
+++ MLRun/build.sh
@@ -6,4 +6,4 @@ pip list | grep mlrun
 #echo "Using API key: $API_KEY" 
 #mlrun project -n cancer-project -u "git://https://github.com/BaldeThierno/MLOps.git" ./project 
 #mlrun project -r main -w ./project/MLRun/cancer-project
-python .MLRun/cancer-project/src/pipeline.py
+python MLRun/cancer-project/src/pipeline.py
