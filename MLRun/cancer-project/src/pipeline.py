fbeb37c86fe363090e956310ade7ea4de0da1e67
Balde, Thierno Abdourahmane <Thierno-Abdourahmane.Balde@adesso-insurance-solutions.de>
2023-05-25 14:56:28 +0200

    new path

--- MLRun/cancer-project/src/pipeline.py
+++ MLRun/cancer-project/src/pipeline.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_breast_cancer
 
 import mlrun
 project = mlrun.get_or_create_project("cancer-project", context="./", user_project=False)
-dataprep_fn = project.set_function("data-prep.py", name="data-prep", kind="job", image="mlrun/mlrun", handler="breast_cancer_generator")
+dataprep_fn = project.set_function("MLRun/cancer-project/src/data-prep.py", name="data-prep", kind="job", image="mlrun/mlrun", handler="breast_cancer_generator")
 run_id = project.run(
     workflow_path="./src/workflow.py",
     arguments={"model_name": "cancer-classifier"},
